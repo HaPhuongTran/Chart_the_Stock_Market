b2ffa1f6816153975195bf7750cbbe4569c72c0f
Phuong Tran Ha <tranhaphuong13@gmail.com>
2018-06-04 18:15:34 +0700

    read java, mysql

--- js/chart.js
+++ js/chart.js
@@ -2,11 +2,11 @@
 
  
 
-var name_stock_array = ["", "VNET","AGTK", "AKAM", "BIDU", "BCOR", "WIFI", "BRNW", "CARB", "JRJC", "CCIH", "CHICF", "CCOI", "CXDO", "CRWG", "EATR", "EDXC",
+var name_stock_array = ["no_name", "VNET","AGTK", "AKAM", "BIDU", "BCOR", "WIFI", "BRNW", "CARB", "JRJC", "CCIH", "CHICF", "CCOI", "CXDO", "CRWG", "EATR", "EDXC",
                         "ENV", "FB", "FLPC", "FZRO", "GEGI", "GDDY", "IAC", "IIJI", "IPAS","JCOM", "LOGL", "LLNW", "MOMO", "NTES", "EGOV", "OTOW", "OPESY",
                         "PTOP", "SIFY", "SINA", "SMCE", "SOHU", "FCCN", "SNST", "TCTZF", "TCEHY", "TMMI", "TRON", "TCX", "TWTR", "WEB", "XNET", "YAHOY", "YNDX"];
 
-var color = ["", "#000000","#2F4F4F", "#990033", "#528B8B", "#EEE8AA", "#8B4513", "#8B795E", "#E0EEE0", "#CDC1C5", "#E6E6FA", 
+var color = ["no_name", "#000000","#2F4F4F", "#990033", "#528B8B", "#EEE8AA", "#8B4513", "#8B795E", "#E0EEE0", "#CDC1C5", "#E6E6FA", 
             "#8470FF", "#473C8B", "#27408B", "#0000EE", "#191970", "#B0E0E6", "#00B2EE", "#00688B", "#6CA6CD", "#E0FFFF",
             "#5F9EA0", "#53868B", "#48D1CC", "#00CED1", "#40E0D0", "#00868B", "#76EEC6", "#9BCD9B", "#2E8B57", "#9AFF9A",
             "#00EE76", "#008B45", "#00CD00", "#008B00", "#006400", "#20B2AA", "#00FA9A", "#ADFF2F", "#B3EE3A", "#556B2F",
@@ -16,9 +16,16 @@ var data_num = [23,45,2,6,1,56,38,23,57,68,43,13,76,34, 100,57,277,345,78,1111,2
 
 
 var ctx = document.getElementById('myChart').getContext('2d');
+
 lable_data = ["January", "February", "March", "April", "May", "June", "July"];
 var data = {
-         labels: lable_data
+         labels: lable_data,
+         datasets:[{
+                    label: "VNET",
+                    backgroundColor: 'rgba(255, 255, 255, 0)',
+                    borderColor: "#000000",
+                    data: [23,45,2,6,1,56,38],
+                }]
 };
 
 var chart = new Chart(ctx, {
@@ -52,8 +59,11 @@ function add(){
             
 
             div_add.classList.add("name_stock");
+            div_add.id = get_name_stock;
             sub_div_add.classList.add("sub_div");
-            button_add.classList.add("close_button");
+            button_add.id = color[i];
+            button_add.classList.add("button_add");
+            
 
             var textnode = document.createTextNode(get_name_stock);
             var text_button = document.createTextNode("X");
@@ -88,12 +98,19 @@ function add(){
 
             document.getElementById("fill_stock").value = '';
             
-            var b_close = document.getElementsByClassName("close_button");
-            var get_class_stock = document.getElementsByClassName("name_stock");
-            b_close[0].addEventListener("click", function(){
-            get_class_stock[0].remove();
-            });
+            break;
         }
-    }    
+    } 
+    var b_close = document.getElementById(color[i]);
+    var get_id_stock = document.getElementById(get_name_stock);
+    b_close.addEventListener("click", function(){
+        get_id_stock.remove();
+        for( var index =0; index< data.datasets.length; index++){
+            if(data.datasets[index].label == get_name_stock){
+                data.datasets.splice(index,1);
+                chart.update();
+            }
+        }
+    });   
 }
 
